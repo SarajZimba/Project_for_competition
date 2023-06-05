ce799b2081ad017cb54506888ccc28050f34a22d
SarajZimba <sarajzimba@gmail.com>
2023-06-05 07:27:59 +0300

    model for rating

--- stroll1/models.py
+++ stroll1/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Customer(models.Model):
-    name = models.CharField(max_length=100)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=True)
+    email = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=100, null=True)
+    review = models.CharField(max_length=1000, null=True, blank=True)
     
+    def __str__(self):
+        return self.name
 
 class Destination(models.Model):
     name = models.CharField(max_length=100)
@@ -55,3 +62,19 @@ class Blogs(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+
+class Rating(models.Model):
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE, related_name= 'rating')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    rating = models.PositiveIntegerField(choices=((1, '1 star'), (2, '2 stars'), (3, '3 stars'), (4, '4 stars'), (5, '5 stars')))
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('destination', 'user')
+
+    def __str__(self):
+        return f"{self.user}'s {self.rating}-star rating for {self.destination}"
+                                         
