db541e1f5273cfa2d3e25585749cfbadef3f278a
SarajZimba <sarajzimba@gmail.com>
2023-06-17 20:39:51 +0300

    dark mode feature added

---
+++ static/js/main.js
@@ -0,0 +1,22 @@
+let i=0;
+
+let icon = document.getElementById('dark')
+
+const change_icon = () =>{
+    if(i==0){
+        i=1;
+        icon.innerHTML="<i class='bi bi-moon on'></i>"
+        document.getElementsByTagName('body')[0].style.backgroundColor = "black"
+        document.getElementsByTagName('body')[0].style.color = "white"
+        document.getElementById('about').style.backgroundColor = "black"
+        document.getElementById('container').style.backgroundColor = "black"
+    }
+    else{
+        i=0;
+        icon.innerHTML="<i class='bi bi-brightness-high on'></i>"
+        document.getElementsByTagName('body')[0].style.backgroundColor = "white"
+        document.getElementsByTagName('body')[0].style.color = "black"
+        document.getElementById('about').style.backgroundColor = "white"
+        document.getElementById('container').style.backgroundColor = "white"
+    }
+}
